24be0bae4d2e0fb1b549cac80ae5aa4d267b1a08
Kavvadias Ioannis <kavvadiasj@hotmail.com>
2018-12-21 07:57:48 +0000

    Remove Lesser Particle Struct

--- Simulation/ParticleCloud.hpp
+++ Simulation/ParticleCloud.hpp
@@ -29,63 +29,6 @@ namespace detail {
   }
 }
 
-
-//this is an itermediate class used to migrate from old system to new.
-//once the migration is done this should be deleted.
-class LesserParticle
-{
-public:
-    //glm::dvec2 position;
-    //glm::dvec2 velocity;
-    //glm::dvec2 normal;
-    //glm::dvec2 Fpress;
-    //glm::dvec2 Fvisc;
-    //glm::dvec2 Fsurf;
-    //glm::dvec2 Fother;
-    //glm::dvec2 Ftot;
-    //double mass;
-    //double density;
-    //double ddensity;    //1./density
-    //double densityErr;
-    //double pressure;
-    //std::vector<Neigbhor> nei; //list of neighbours
-
-public:
-    LesserParticle()
-    //position(-100.0),
-    //velocity(0.0),
-    //normal(0.0),
-    //Fpress(0.0),
-    //Fvisc(0.0),
-    //Fsurf(0.0),
-    //Fother(0.0),
-    //Ftot(0.0),
-    //mass(0.0),
-    //density(0.0),
-    //ddensity(0.0),
-    //densityErr(0.0),
-    //pressure(0.0),
-    //nei()
-    {}
-
-    LesserParticle(const Particle&)
-    //position   (p.position),
-    //velocity   (p.velocity),
-    //normal     (p.normal),
-    //Fpress     (p.Fpress),
-    //Fvisc      (p.Fvisc),
-    //Fsurf      (p.Fsurf),
-    //Fother     (p.Fother),
-    //Ftot       (p.Ftot),
-    //mass       (p.mass),
-    //density    (p.density),
-    //ddensity   (p.ddensity),
-    //densityErr (p.densityErr),
-    //pressure   (p.pressure),
-    //nei        (p.nei)
-    {}
-};
-
 //order must be in aggrement with data tuple
 enum class Attr : size_t {
   ePosition = 0,
@@ -127,23 +70,7 @@ private:
     static constexpr size_t nAttributes = std::tuple_size<DataType>::value;
 public:
 
-    using ParticleVector = std::vector<LesserParticle>;
-    using iterator = ParticleVector::iterator;
-    using const_iterator = ParticleVector::const_iterator;
-
-          iterator  begin()       { return _cloud.begin(); }
-    const_iterator  begin() const { return _cloud.begin(); }
-    const_iterator cbegin() const { return _cloud.cbegin(); }
-          iterator  end()       { return _cloud.end(); }
-    const_iterator  end() const { return _cloud.end(); }
-    const_iterator cend() const { return _cloud.cend(); }
-
-          LesserParticle& front()       { return _cloud.front(); }
-    const LesserParticle& front() const { return _cloud.front(); }
-          LesserParticle& back()       { return _cloud.back(); }
-    const LesserParticle& back() const { return _cloud.back(); }
-
-    ParticleCloud(): _cloud(), _data() {}
+    ParticleCloud(): _data() {}
     ParticleCloud(const ParticleCloud&) = default;
     ParticleCloud(ParticleCloud&&)      = default;
 
@@ -151,9 +78,6 @@ public:
 
     ParticleCloud& operator=(const ParticleCloud&) = default;
     ParticleCloud& operator=(ParticleCloud&&)      = default;
-
-    LesserParticle& operator[](size_t i) { return _cloud[i]; }
-    const LesserParticle& operator[](size_t i) const { return _cloud[i]; }
 
     template <Attr attr>
     decltype(auto) get() { return std::get<static_cast<size_t>(attr)>(_data); }
@@ -166,7 +90,6 @@ public:
     }
 
     void push_back(const Particle& p) {
-      _cloud.push_back(LesserParticle{p});
       get<Attr::ePosition>().push_back(p.position);
       get<Attr::eVelocity>().push_back(p.velocity);
       get<Attr::eNormal>().push_back(p.normal);
@@ -203,26 +126,20 @@ public:
       };
     }
 
-    size_t size() const { return _cloud.size(); }
+    size_t size() const { return get<Attr::ePosition>().size(); }
 
-    bool empty() const { return _cloud.empty(); }
-
-    ParticleVector& getCloud() { return _cloud; }
-    const ParticleVector& getCloud() const { return _cloud; }
+    bool empty() const { return get<Attr::ePosition>().empty(); }
 
     template <class Functor>
     void applyFunctor(const Functor& f) {
-      f(_cloud);
       detail::applyOnTuple(_data, f, std::make_index_sequence<nAttributes>());
     }
     template <class Functor>
     void applyFunctor(const Functor& f) const {
-      f(_cloud);
       detail::applyOnTuple(_data, f, std::make_index_sequence<nAttributes>());
     }
 
 private:
-    ParticleVector _cloud;
     DataType _data; 
 };
 
